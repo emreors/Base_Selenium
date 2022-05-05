8c4dd451afb1fc7c7d4b4fd5bbe4606858009e36
emreors <emreors1987@gmail.com>
2022-05-05 17:32:50 +0300

    cucumber güncellemeler

--- selenium/src/test/java/testAutomations/stepsDefinitionForCucumber/CucumberExample.java
+++ selenium/src/test/java/testAutomations/stepsDefinitionForCucumber/CucumberExample.java
@@ -3,6 +3,7 @@ package testAutomations.stepsDefinitionForCucumber;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.PageFactory;
 import testAutomations.Kullanici;
@@ -16,30 +17,33 @@ public class CucumberExample extends TestBase {
 
     private String baseUrl = servisAdresi;
     JavascriptExecutor js = (JavascriptExecutor) driver;
+    @Test
 
     @Given("Tanimli URL acilir.")
-    public void tanimli_url_acilir() {
+    public void testtanimli_url_acilir() {
         PageFactory.initElements(driver, this);
 
     }
 
     @Given("Kullanici adi ve sifre ile sisteme giris yapilir.")
-    public void kullanici_adi_ve_sifre_ile_sisteme_giris_yapilir() {
+    public void testkullanici_adi_ve_sifre_ile_sisteme_giris_yapilir() {
         try {
             kullaniciGirisiYap(driver, Kullanici.KULLANICI3);
         } catch (Exception e) {
             System.out.println("Zaten giriş yapılmış.");
         }
-        
+
     }
+
     @When("Anasayfada ust kisimda bulunan arama yap alanina tiklanir.")
     public void anasayfada_ust_kisimda_bulunan_arama_yap_alanina_tiklanir() {
         // Write code here that turns the phrase above into concrete actions
-        
+
     }
+
     @Then("Cikis yapilir")
     public void cikis_yapilir() {
         // Write code here that turns the phrase above into concrete actions
-        
+
     }
 }
